b47c2f5ced30b28ad335874cf75c58d4ca11476e
vuvanminh <vuvanminh2612003@gmail.com>
2023-10-01 13:11:29 +0700

    Add file calculator winform

---
+++ Calculator/MaSV_HoTen_BTLCN1_Calculator/Form1.Designer.cs
@@ -0,0 +1,411 @@
+﻿namespace MaSV_HoTen_BTLCN1_Calculator
+{
+    partial class Form1
+    {
+
+
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            btn_2 = new Button();
+            btn_chia = new Button();
+            btn_1 = new Button();
+            btn_nhan = new Button();
+            btn_cong = new Button();
+            btn_5 = new Button();
+            btn_tru = new Button();
+            btn_4 = new Button();
+            btn_6 = new Button();
+            btn_9 = new Button();
+            btn_7 = new Button();
+            btn_8 = new Button();
+            btn_0 = new Button();
+            btn_3 = new Button();
+            btn_cham = new Button();
+            btn_bang = new Button();
+            btn_xoa = new Button();
+            btn_percent = new Button();
+            btn_clear = new Button();
+            btn_clearentry = new Button();
+            txt_kq = new TextBox();
+            btn_amduong = new Button();
+            txt_kq2 = new TextBox();
+            btn_lichsu = new Button();
+            SuspendLayout();
+            // 
+            // btn_2
+            // 
+            btn_2.BackColor = Color.SteelBlue;
+            btn_2.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            btn_2.Location = new Point(115, 245);
+            btn_2.Name = "btn_2";
+            btn_2.Size = new Size(75, 51);
+            btn_2.TabIndex = 1;
+            btn_2.Text = "2";
+            btn_2.UseVisualStyleBackColor = false;
+            btn_2.Click += Number_click;
+            // 
+            // btn_chia
+            // 
+            btn_chia.BackColor = Color.LightCyan;
+            btn_chia.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            btn_chia.Location = new Point(304, 360);
+            btn_chia.Name = "btn_chia";
+            btn_chia.Size = new Size(79, 51);
+            btn_chia.TabIndex = 2;
+            btn_chia.Text = "/";
+            btn_chia.UseVisualStyleBackColor = false;
+            btn_chia.Click += toantu_click;
+            // 
+            // btn_1
+            // 
+            btn_1.BackColor = Color.SteelBlue;
+            btn_1.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            btn_1.Location = new Point(24, 245);
+            btn_1.Name = "btn_1";
+            btn_1.Size = new Size(71, 51);
+            btn_1.TabIndex = 3;
+            btn_1.Text = "1";
+            btn_1.UseVisualStyleBackColor = false;
+            btn_1.Click += Number_click;
+            // 
+            // btn_nhan
+            // 
+            btn_nhan.BackColor = Color.LightCyan;
+            btn_nhan.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            btn_nhan.Location = new Point(304, 303);
+            btn_nhan.Name = "btn_nhan";
+            btn_nhan.Size = new Size(79, 51);
+            btn_nhan.TabIndex = 3;
+            btn_nhan.Text = "x";
+            btn_nhan.UseVisualStyleBackColor = false;
+            btn_nhan.Click += toantu_click;
+            // 
+            // btn_cong
+            // 
+            btn_cong.BackColor = Color.LightCyan;
+            btn_cong.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            btn_cong.Location = new Point(304, 188);
+            btn_cong.Name = "btn_cong";
+            btn_cong.Size = new Size(79, 51);
+            btn_cong.TabIndex = 3;
+            btn_cong.Text = "+";
+            btn_cong.UseVisualStyleBackColor = false;
+            btn_cong.Click += toantu_click;
+            // 
+            // btn_5
+            // 
+            btn_5.BackColor = Color.SteelBlue;
+            btn_5.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            btn_5.Location = new Point(115, 302);
+            btn_5.Name = "btn_5";
+            btn_5.Size = new Size(75, 52);
+            btn_5.TabIndex = 3;
+            btn_5.Text = "5";
+            btn_5.UseVisualStyleBackColor = false;
+            btn_5.Click += Number_click;
+            // 
+            // btn_tru
+            // 
+            btn_tru.BackColor = Color.LightCyan;
+            btn_tru.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            btn_tru.Location = new Point(304, 245);
+            btn_tru.Name = "btn_tru";
+            btn_tru.Size = new Size(79, 51);
+            btn_tru.TabIndex = 3;
+            btn_tru.Text = "-";
+            btn_tru.UseVisualStyleBackColor = false;
+            btn_tru.Click += toantu_click;
+            // 
+            // btn_4
+            // 
+            btn_4.BackColor = Color.SteelBlue;
+            btn_4.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            btn_4.Location = new Point(24, 302);
+            btn_4.Name = "btn_4";
+            btn_4.Size = new Size(71, 52);
+            btn_4.TabIndex = 0;
+            btn_4.Text = "4";
+            btn_4.UseVisualStyleBackColor = false;
+            btn_4.Click += Number_click;
+            // 
+            // btn_6
+            // 
+            btn_6.BackColor = Color.SteelBlue;
+            btn_6.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            btn_6.Location = new Point(212, 302);
+            btn_6.Name = "btn_6";
+            btn_6.Size = new Size(75, 52);
+            btn_6.TabIndex = 1;
+            btn_6.Text = "6";
+            btn_6.UseVisualStyleBackColor = false;
+            btn_6.Click += Number_click;
+            // 
+            // btn_9
+            // 
+            btn_9.BackColor = Color.SteelBlue;
+            btn_9.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            btn_9.Location = new Point(212, 360);
+            btn_9.Name = "btn_9";
+            btn_9.Size = new Size(75, 51);
+            btn_9.TabIndex = 2;
+            btn_9.Text = "9";
+            btn_9.UseVisualStyleBackColor = false;
+            btn_9.Click += Number_click;
+            // 
+            // btn_7
+            // 
+            btn_7.BackColor = Color.SteelBlue;
+            btn_7.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            btn_7.Location = new Point(24, 360);
+            btn_7.Name = "btn_7";
+            btn_7.Size = new Size(71, 51);
+            btn_7.TabIndex = 3;
+            btn_7.Text = "7";
+            btn_7.UseVisualStyleBackColor = false;
+            btn_7.Click += Number_click;
+            // 
+            // btn_8
+            // 
+            btn_8.BackColor = Color.SteelBlue;
+            btn_8.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            btn_8.Location = new Point(115, 360);
+            btn_8.Name = "btn_8";
+            btn_8.Size = new Size(75, 51);
+            btn_8.TabIndex = 3;
+            btn_8.Text = "8";
+            btn_8.UseVisualStyleBackColor = false;
+            btn_8.Click += Number_click;
+            // 
+            // btn_0
+            // 
+            btn_0.BackColor = Color.SteelBlue;
+            btn_0.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            btn_0.Location = new Point(24, 417);
+            btn_0.Name = "btn_0";
+            btn_0.Size = new Size(166, 54);
+            btn_0.TabIndex = 3;
+            btn_0.Text = "0";
+            btn_0.UseVisualStyleBackColor = false;
+            btn_0.Click += Number_click;
+            // 
+            // btn_3
+            // 
+            btn_3.BackColor = Color.SteelBlue;
+            btn_3.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            btn_3.Location = new Point(212, 245);
+            btn_3.Name = "btn_3";
+            btn_3.Size = new Size(75, 51);
+            btn_3.TabIndex = 3;
+            btn_3.Text = "3";
+            btn_3.UseVisualStyleBackColor = false;
+            btn_3.Click += Number_click;
+            // 
+            // btn_cham
+            // 
+            btn_cham.BackColor = Color.SteelBlue;
+            btn_cham.Location = new Point(212, 420);
+            btn_cham.Name = "btn_cham";
+            btn_cham.Size = new Size(75, 51);
+            btn_cham.TabIndex = 4;
+            btn_cham.Text = ".";
+            btn_cham.UseVisualStyleBackColor = false;
+            btn_cham.Click += cham_click;
+            // 
+            // btn_bang
+            // 
+            btn_bang.BackColor = Color.LightCyan;
+            btn_bang.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            btn_bang.Location = new Point(304, 420);
+            btn_bang.Name = "btn_bang";
+            btn_bang.Size = new Size(79, 51);
+            btn_bang.TabIndex = 5;
+            btn_bang.Text = "=";
+            btn_bang.UseVisualStyleBackColor = false;
+            btn_bang.Click += btn_bang_Click;
+            // 
+            // btn_xoa
+            // 
+            btn_xoa.BackColor = Color.CornflowerBlue;
+            btn_xoa.BackgroundImage = Properties.Resources.delete;
+            btn_xoa.BackgroundImageLayout = ImageLayout.Zoom;
+            btn_xoa.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            btn_xoa.Location = new Point(304, 131);
+            btn_xoa.Name = "btn_xoa";
+            btn_xoa.Size = new Size(79, 51);
+            btn_xoa.TabIndex = 6;
+            btn_xoa.UseVisualStyleBackColor = false;
+            btn_xoa.Click += btn_xoa_Click;
+            // 
+            // btn_percent
+            // 
+            btn_percent.BackColor = Color.LightCyan;
+            btn_percent.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            btn_percent.Location = new Point(24, 188);
+            btn_percent.Name = "btn_percent";
+            btn_percent.Size = new Size(71, 51);
+            btn_percent.TabIndex = 7;
+            btn_percent.Text = "%";
+            btn_percent.UseVisualStyleBackColor = false;
+            btn_percent.Click += btn_percent_Click;
+            // 
+            // btn_clear
+            // 
+            btn_clear.BackColor = Color.LightGray;
+            btn_clear.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            btn_clear.Location = new Point(212, 188);
+            btn_clear.Name = "btn_clear";
+            btn_clear.Size = new Size(75, 51);
+            btn_clear.TabIndex = 8;
+            btn_clear.Text = "C";
+            btn_clear.UseVisualStyleBackColor = false;
+            btn_clear.Click += btn_clear_Click;
+            // 
+            // btn_clearentry
+            // 
+            btn_clearentry.BackColor = Color.LightGray;
+            btn_clearentry.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            btn_clearentry.Location = new Point(115, 188);
+            btn_clearentry.Name = "btn_clearentry";
+            btn_clearentry.Size = new Size(75, 51);
+            btn_clearentry.TabIndex = 9;
+            btn_clearentry.Text = "CE";
+            btn_clearentry.UseVisualStyleBackColor = false;
+            btn_clearentry.Click += btn_clearentry_Click;
+            // 
+            // txt_kq
+            // 
+            txt_kq.BackColor = SystemColors.Info;
+            txt_kq.CharacterCasing = CharacterCasing.Upper;
+            txt_kq.Font = new Font("Segoe UI", 24F, FontStyle.Bold, GraphicsUnit.Point);
+            txt_kq.Location = new Point(24, 41);
+            txt_kq.Multiline = true;
+            txt_kq.Name = "txt_kq";
+            txt_kq.Size = new Size(359, 81);
+            txt_kq.TabIndex = 10;
+            txt_kq.TextAlign = HorizontalAlignment.Right;
+            txt_kq.Click += Number_click;
+            // 
+            // btn_amduong
+            // 
+            btn_amduong.BackColor = Color.LightCyan;
+            btn_amduong.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            btn_amduong.Location = new Point(24, 131);
+            btn_amduong.Name = "btn_amduong";
+            btn_amduong.Size = new Size(71, 51);
+            btn_amduong.TabIndex = 11;
+            btn_amduong.Text = "+ / -";
+            btn_amduong.UseVisualStyleBackColor = false;
+            btn_amduong.Click += btn_amduong_Click;
+            // 
+            // txt_kq2
+            // 
+            txt_kq2.BackColor = Color.DarkOrange;
+            txt_kq2.BorderStyle = BorderStyle.None;
+            txt_kq2.Location = new Point(237, 11);
+            txt_kq2.Name = "txt_kq2";
+            txt_kq2.Size = new Size(146, 16);
+            txt_kq2.TabIndex = 12;
+            txt_kq2.TextAlign = HorizontalAlignment.Right;
+            // 
+            // btn_lichsu
+            // 
+            btn_lichsu.BackgroundImage = Properties.Resources.history;
+            btn_lichsu.BackgroundImageLayout = ImageLayout.Stretch;
+            btn_lichsu.Location = new Point(208, 9);
+            btn_lichsu.Name = "btn_lichsu";
+            btn_lichsu.Size = new Size(24, 19);
+            btn_lichsu.TabIndex = 13;
+            btn_lichsu.UseVisualStyleBackColor = true;
+            btn_lichsu.Click += btn_lichsu_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.LightBlue;
+            ClientSize = new Size(400, 485);
+            Controls.Add(btn_lichsu);
+            Controls.Add(txt_kq2);
+            Controls.Add(btn_amduong);
+            Controls.Add(txt_kq);
+            Controls.Add(btn_clearentry);
+            Controls.Add(btn_clear);
+            Controls.Add(btn_percent);
+            Controls.Add(btn_xoa);
+            Controls.Add(btn_bang);
+            Controls.Add(btn_cham);
+            Controls.Add(btn_0);
+            Controls.Add(btn_nhan);
+            Controls.Add(btn_8);
+            Controls.Add(btn_tru);
+            Controls.Add(btn_3);
+            Controls.Add(btn_5);
+            Controls.Add(btn_7);
+            Controls.Add(btn_cong);
+            Controls.Add(btn_9);
+            Controls.Add(btn_1);
+            Controls.Add(btn_6);
+            Controls.Add(btn_chia);
+            Controls.Add(btn_4);
+            Controls.Add(btn_2);
+            Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            Name = "Form1";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Calculator";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+        private Button btn_2;
+        private Button btn_chia;
+        private Button btn_1;
+        private Button btn_nhan;
+        private Button btn_cong;
+        private Button btn_5;
+        private Button btn_tru;
+        private Button btn_4;
+        private Button btn_6;
+        private Button btn_9;
+        private Button btn_7;
+        private Button btn_8;
+        private Button btn_0;
+        private Button btn_3;
+        private Button btn_cham;
+        private Button btn_bang;
+        private Button btn_xoa;
+        private Button btn_percent;
+        private Button btn_clear;
+        private Button btn_clearentry;
+        private TextBox txt_kq;
+        private Button btn_amduong;
+        private TextBox txt_kq2;
+        private Button btn_lichsu;
+    }
+}
---
+++ Calculator/MaSV_HoTen_BTLCN1_Calculator/Form1.cs
@@ -0,0 +1,170 @@
+﻿using System.Text;
+
+namespace MaSV_HoTen_BTLCN1_Calculator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            history = new List<string>();
+        }
+        #region bienvariable
+        string _toantu = "";
+        bool toantu_Clicked = false;
+        double a = 0;
+        private List<string> history;
+        #endregion
+        #region Method
+        private void FormatNumberWithCommas()
+        {
+            string input = txt_kq.Text;
+
+            if (int.TryParse(input, out int number))
+            {
+                string formattedNumber = number.ToString("#,###");
+
+                txt_kq.Text = formattedNumber;
+            }
+        }
+        private void ShowHistory()
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (string calculation in history)
+            {
+                sb.AppendLine(calculation);
+            }
+
+            MessageBox.Show(sb.ToString(), "Lịch sử các phép tính");
+        }
+        #endregion
+        private void Number_click(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            if (toantu_Clicked)
+            {
+                txt_kq.Text = btn.Text;
+                toantu_Clicked = false;
+            }
+            else
+            {
+                txt_kq.Text += btn.Text;
+            }
+            this.txt_kq.SelectionStart = txt_kq.Text.Length;
+        }
+        private void toantu_click(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            if (!string.IsNullOrEmpty(txt_kq.Text))
+            {
+                a = double.Parse(txt_kq.Text);
+                _toantu = btn.Text;
+                txt_kq2.Text = txt_kq.Text + " " + _toantu;
+                toantu_Clicked = true;
+            }
+        }
+
+        private void cham_click(object sender, EventArgs e)
+        {
+            if (!txt_kq.Text.Contains("."))
+            {
+                Button btn = (Button)sender;
+                txt_kq.Text += btn.Text;
+                this.txt_kq.SelectionStart = txt_kq.Text.Length;
+            }
+        }
+
+        private void btn_clear_Click(object sender, EventArgs e)
+        {
+            txt_kq.Text = "";
+            txt_kq2.Text = "";
+            btn_lichsu.Text = "";
+            a = 0;
+            _toantu = "";
+
+            toantu_Clicked = false;
+        }
+        private void btn_clearentry_Click(object sender, EventArgs e)
+        {
+            txt_kq.Text = "";
+            txt_kq2.Text = "";
+        }
+
+        private void btn_xoa_Click(object sender, EventArgs e)
+        {
+            if (txt_kq.Text != "")
+            {
+                txt_kq.Text = txt_kq.Text.Remove(txt_kq.Text.Length - 1, 1);
+                txt_kq.SelectionStart = txt_kq.Text.Length;
+            }
+        }
+
+
+        private void btn_amduong_Click(object sender, EventArgs e)
+        {
+            if (txt_kq.Text[0] == '-')
+            {
+                txt_kq.Text = txt_kq.Text.TrimStart('-');
+            }
+            else
+            {
+                txt_kq.Text = '-' + txt_kq.Text;
+            }
+        }
+        private bool isLastCalculationAdded = false;
+
+        private void btn_bang_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(txt_kq.Text))
+            {
+                double b = double.Parse(txt_kq.Text);
+                double ketqua = 0;
+                switch (_toantu)
+                {
+                    case "+":
+                        ketqua = a + b;
+                        break;
+                    case "-":
+                        ketqua = a - b;
+                        break;
+                    case "x":
+                        ketqua = a * b;
+                        break;
+                    case "/":
+                        ketqua = a / b;
+                        break;
+                }
+                txt_kq.Text = ketqua.ToString();
+                txt_kq2.Text = txt_kq2.Text + " " + b + " = " + ketqua;
+                toantu_Clicked = false;
+
+                //them vao lich su
+                string calculation = txt_kq2.Text + " " + txt_kq.Text + " = " + ketqua;
+                history.Add(calculation);
+                //
+
+            }
+        }
+
+        private void btn_percent_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(txt_kq.Text))
+            {
+                double currentValue = Convert.ToDouble(txt_kq.Text);
+                double percentageValue = currentValue / 100;
+                txt_kq.Text = percentageValue.ToString();
+                txt_kq2.Text = txt_kq.Text + " %";
+            }
+        }
+
+        private void txt_kq_TextChanged(object sender, EventArgs e)
+        {
+            FormatNumberWithCommas();
+        }
+
+        private void btn_lichsu_Click(object sender, EventArgs e)
+        {
+            ShowHistory();
+        }
+    }
+}
